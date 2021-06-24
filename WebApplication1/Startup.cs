1c649aa383c8ea194951515ab82e50ff5795bf57
aiwell <122696321@qq.com>
2021-06-24 16:15:10 +0800

    提交修改

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -21,6 +21,7 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //Ê×´ÎÐÞ¸Ä
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
